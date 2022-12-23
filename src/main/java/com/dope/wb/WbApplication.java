9ee3fea0b6782e6ab7c211291977b4fdd92af057
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-12-23 18:32:26 +0900

    init

---
+++ src/main/java/com/dope/wb/WbApplication.java
@@ -0,0 +1,13 @@
+package com.dope.wb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbApplication.class, args);
+	}
+
+}
