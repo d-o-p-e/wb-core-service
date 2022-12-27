13a2e37fdfd2ea54541fce142f07aca91efe2222
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-12-27 15:32:45 +0900

    feat: add spring security configure

---
+++ src/main/java/com/dope/wb/security/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.dope.wb.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm");
+        return http.build();
+    }
+
+}
