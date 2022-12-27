7a5cfd233c7f8e6cf8a5a5119b9fe42c66a380e3
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-12-28 01:48:08 +0900

    feat: add password encoder for sign up

--- src/main/java/com/dope/wb/service/UserService.java
+++ src/main/java/com/dope/wb/service/UserService.java
@@ -3,16 +3,24 @@ package com.dope.wb.service;
 import com.dope.wb.domain.user.User;
 import com.dope.wb.dto.JoinRequestDto;
 import com.dope.wb.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void joinUser(JoinRequestDto joinRequestDto) {
+        joinRequestDto.setPassword(bCryptPasswordEncoder.encode(joinRequestDto.getPassword()));
         User user = User.createNewUser(joinRequestDto);
         userRepository.save(user);
     }
