ba17c1bfb57dad15eef051d986ad4c5d0797f95b
JinuCheon <woojin8787@gmail.com>
2022-12-31 01:11:50 +0900

    feat: upload product sketch

--- src/main/java/com/dope/wb/service/ProductService.java
+++ src/main/java/com/dope/wb/service/ProductService.java
@@ -2,9 +2,11 @@ package com.dope.wb.service;
 
 import com.dope.wb.domain.product.Product;
 import com.dope.wb.domain.product.ProductImage;
+import com.dope.wb.domain.product.ProductSketch;
 import com.dope.wb.dto.ProductUploadRequestDto;
 import com.dope.wb.repository.ProductImageRepository;
 import com.dope.wb.repository.ProductRepository;
+import com.dope.wb.repository.ProductSketchRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,24 +15,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-    @Value("${app.resource.product.image}")
-    private String productImageBaseDir;
-
     private final ProductRepository productRepository;
     private final ProductImageRepository productImageRepository;
+    private final ProductSketchRepository productSketchRepository;
+
+    @Value("${app.resource.product.image}")
+    private String productImageBaseDir;
+    @Value("${app.resource.product.sketch}")
+    private String productSketchBaseDir;
 
     @Transactional
     public void uploadNewProduct(ProductUploadRequestDto productUploadRequestDto) {
@@ -42,36 +44,36 @@ public class ProductService {
         productRepository.save(product);
 
         uploadProductImages(product, productUploadRequestDto.getImages());
+        uploadProductSketch(product, productUploadRequestDto.getSketch());
     }
 
     private void uploadProductImages(Product product, List<MultipartFile> images) {
-        String baseDir = productImageBaseDir + File.separator;
-
-        if (!Files.exists(Paths.get(baseDir))) {
-            try {
-                Files.createDirectories(Paths.get(baseDir));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
         for (MultipartFile image : images) {
             ProductImage productImage = ProductImage.builder().product(product).build();
             productImageRepository.save(productImage);
-            String filename = image.getOriginalFilename();
-            String extension = filename.substring(filename.indexOf(".")).toLowerCase(Locale.ROOT);
-            if(extension.equals("jpg") || extension.equals("png") || extension.equals("gif")) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "not supported file");
-            }
+            Path filePath = productImage.createFilePath(product, image, productImageBaseDir);
+            productImage.setPath(filePath.toAbsolutePath().toString());
 
             try {
-                Path copyOfLocation = Paths.get(baseDir + product.getSerial() + productImage.getId() + extension);
-                Files.copy(image.getInputStream(), copyOfLocation, StandardCopyOption.REPLACE_EXISTING);
-                productImage.setNewFilePath(copyOfLocation.toAbsolutePath().toString());
+                Files.copy(image.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
                 e.printStackTrace();
                 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
             }
+        }
+    }
+
+    private void uploadProductSketch(Product product, MultipartFile sketch) {
+        ProductSketch productSketch = ProductSketch.builder().product(product).build();
+        productSketchRepository.save(productSketch);
+        Path filePath = productSketch.createFilePath(product, sketch, productSketchBaseDir);
+        productSketch.setPath(filePath.toAbsolutePath().toString());
+
+        try {
+            Files.copy(sketch.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
