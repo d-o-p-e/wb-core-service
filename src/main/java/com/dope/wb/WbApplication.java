87b51c0b3bb14192bbf7a6eae50efcac555cd5e4
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-01-11 02:04:15 +0900

    add specifications for running on external tomcat servlets

--- src/main/java/com/dope/wb/WbApplication.java
+++ src/main/java/com/dope/wb/WbApplication.java
@@ -2,10 +2,16 @@ package com.dope.wb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WbApplication {
+public class WbApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WbApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WbApplication.class, args);
 	}
